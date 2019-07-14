f5058b1a24465c6f73d8e766def50a0d7a4ff708
v1a1l1e1 <valentina.lore84@gmail.com>
2019-07-14 10:36:37 +0200

    primo commit

---
+++ i2c.h
@@ -0,0 +1,52 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <util/twi.h>
+#include <avr/interrupt.h>
+
+/* defs */
+
+#define SDA_W() DDRC |=  (1 << PC4) // output
+#define SDA_R() DDRC &= ~(1 << PC4) // input
+#define TWBR_V 32
+
+/* dichiarazioni */
+
+void i2c_init(void);
+void i2c_start(void);
+void i2c_send_stop(uint8_t addr);
+void i2c_tx_data (uint8_t data);
+void i2c_rx_data(uint8_t flag);
+uint8_t i2c_get_data(void);
+uint8_t i2c_get_status(void);
+
+/*definizioni */
+
+void i2c_init(void){
+	/*
+	  ------------------------------------------
+	  | freq = F_CPU / (16 + 2*TWBR*prescaler) |
+	  ------------------------------------------
+		F_CPU = 8MHz
+		TWBR = 32
+		freq = 8MHz/(16+2*32) ==> ~100KHz
+	*/		
+	TWBR = TWBR_V; // 
+	TWSR &= (~(1<<TWPS0) | ~(1 << TWPS1)); // prescaler = 1
+	TWCR |= (1 << TWEN);
+}
+
+void i2c_start(void){
+	SDA_W();
+	TWCR = (1<<TWINT) | (1 << TWEN) | (1 << TWSTA);
+	while ((TWCR & (1 << TWINT)) == 0) 
+		;
+}
+
+void i2c_stop(void){
+	SDA_W();
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+}
+
+
+
+	
